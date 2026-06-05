69e605017ee797e046c25e91a0e11b2a23ba7a3a
riannadev <riannaknogl@gmail.com>
2026-06-05 11:06:46 -0400

    Update EasyGrid.java

--- EasyGrid.java
+++ EasyGrid.java
@@ -16,6 +16,6 @@ public class EasyGrid extends World
     public EasyGrid()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(20, 20, 30); 
     }
 }
