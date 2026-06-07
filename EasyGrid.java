b3df8485e5c06f5470cfdb8e3dce782442ef4a73
Neha <107941977+Nehaparmar1@users.noreply.github.com>
2026-06-07 14:19:34 -0400

    Update EasyGrid.java

--- EasyGrid.java
+++ EasyGrid.java
@@ -15,7 +15,7 @@ public class EasyGrid extends World
      */
     public EasyGrid()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        addObject(new Snake(), 300, 300);
     }
 }
