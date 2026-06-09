6e0f264733dff4c2d2a9367f9d420fcf1f165fb8
riannadev <riannaknogl@gmail.com>
2026-06-09 12:44:31 -0400

    audio

--- MyWorld.java
+++ MyWorld.java
@@ -5,16 +5,44 @@ public class MyWorld extends World
     private int time = 0;
     private int seconds = 0;
     
+    // LOAD THE BACKGROUND MUSIC
+    private GreenfootSound bgMusic = new GreenfootSound("background_music.wav");
+    
     public MyWorld()
     {
         super(600, 300, 1);
-        
         setBackground("backgroundSnake.png");
-
     } 
+    
+    // Fallback native checks
+    public void started()
+    {
+        bgMusic.playLoop();
+    }
+
+    public void stopped()
+    {
+        bgMusic.pause();
+    }
+
+    // A method so the snake can turn off the music when it dies
+    public void stopBackgroundMusic()
+    {
+        if (bgMusic.isPlaying())
+        {
+            bgMusic.stop();
+        }
+    }
     
     public void act()
     {
+        // --- BULLETPROOF AUDIO FALLBACK ---
+        // If the game is running but the music hasn't started yet, force play it!
+        if (!bgMusic.isPlaying()) 
+        {
+            bgMusic.playLoop();
+        }
+
         time++;
     
         if(time % 60 == 0)
